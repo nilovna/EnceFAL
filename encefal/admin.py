c134b313b251fad2ad6e7570744610d51bc1e920
Nilovna Bascunan-Vasquez <contact@nilovna.com>
2011-07-20 01:18:21 -0400

    Add link that lists all books of a Vendeur (seller)

--- encefal/admin.py
+++ encefal/admin.py
@@ -1,18 +1,25 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+
 from encefal.models import Vendeur, Session, Livre
 
 class VendeurAdmin(admin.ModelAdmin):
    exclude = ('actif',)
-   list_display = ('_vendeur', 'code_permanent', 'telephone', 'email', )
+   list_display = ('_vendeur', 'code_permanent', 'telephone', 'email', 
+                     'liste_livres',)
    search_fields = ['nom', 'prenom']
 
    def _vendeur(self, obj):
       return obj
    _vendeur.short_description = "Vendeur"
 
-   # TODO: Voir la liste de tous les livres du vendeur
-   
+   # Permet de lister tous les livres d'un vendeur
+   def liste_livres(self, obj):
+      return "<a href='%s?vendeur__id__exact=%s'>Voir les livres\
+               </a>" % (reverse('admin:encefal_livre_changelist'), obj.id)
+   liste_livres.allow_tags = True 
+   liste_livres.short_description = "Liste des livres du vendeur"
 
 class SessionAdmin(admin.ModelAdmin):
    exclude = ('actif',)
