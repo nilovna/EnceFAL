135e208c174dd566d837acb99944ace244db9155
louib <louisbvarin@gmail.com>
2013-06-19 22:01:01 -0400

    Fixed simplejson -> json et README.md minimal.

--- project/encefal/models.py
+++ project/encefal/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.forms import ValidationError
 import urllib
-import simplejson
+import json
 
 ################################################################################
 # CONSTANTES (CONSTANTS)
