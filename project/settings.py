e1b37a7924f4def33285e0ef20abda1945936a39
louib <louisbvarin@gmail.com>
2013-08-27 21:09:43 -0400

    added fixtures for group employe

--- project/settings.py
+++ project/settings.py
@@ -72,6 +72,8 @@ DEBUG=False
 TEMPLATE_DEBUG=DEBUG
 AUTH_PASSWORD_REQUIRED = False
 
+LOGIN_REDIRECT_URL = "/employee/"
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
