404f909851dc706834cece32656133070641c1d4
louib <louisbvarin@gmail.com>
2013-09-01 16:00:25 -0400

    deleted paiement view

--- project/encefal/views.py
+++ project/encefal/views.py
@@ -30,23 +30,6 @@ def index(request):
     return render_to_response('admin', {},
                               RequestContext(request))
 
-def paiement(request):
-    facture = Facture.objects.get(id=request.GET.get('id'))
-    livres = facture.livres.all()
-    if request.method == "POST":
-        for livre in livres:
-            # Changer le statut des livres à vendu.
-            livre.etat = ETAT_LIVRE_CHOICES[1][0]
-            livre.save()
-        return HttpResponseRedirect(reverse('admin:encefal_livre_changelist'))
-    else:
-        prix = livres.aggregate(total=Sum('prix'))
-
-    return render_to_response('encefal/paiement.html',
-                              {'facture':facture, 'livres':livres,
-                              'total': prix['total']},
-                              context_instance = RequestContext(request))
-
 def vendre(request):
     livre_ids = request.GET.get('ids').split(',')
     livres = Livre.objects.filter(id__in=livre_ids)
